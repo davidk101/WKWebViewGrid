b0728f41519ec2c5befa7dc75ddb42439d7d3c4c
David Kumar <david.dn.kumar@gmail.com>
2021-07-24 13:09:49 -0500

    Initial Commit

---
+++ wkwebviewgrid/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  wkwebviewgrid
+//
+//  Created by David Kumar on 7/24/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
