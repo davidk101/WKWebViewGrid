7f4a0426c011a60333f9a84f87a65981b48cfa9e
David Kumar <david.dn.kumar@gmail.com>
2021-07-24 14:00:46 -0500

    add ui elements to window

---
+++ wkwebviewgrid/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  wkwebviewgrid
+//
+//  Created by David Kumar on 7/24/21.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        window?.titleVisibility = .hidden // toolbar moved up to window traffic lights
+
+    }
+
+}
